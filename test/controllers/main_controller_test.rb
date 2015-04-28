f5f6b3c58e31485c785e0066acf86e917061ec2c
jleeothon <jleeothon@gmail.com>
2015-04-27 23:02:03 -0500

    Add tests

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -4,4 +4,20 @@ class MainControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "result renders correct description" do
+    query = {
+      "sex"=>"F", "age"=>"15", "address"=>"U", "famsize"=>"LE3", "Pstatus"=>"T",
+      "Medu"=>"0", "Fedu"=>"0", "Fjob"=>"teacher", "reason"=>"home",
+      "guardian"=>"mother", "traveltime"=>"1", "studytime"=>"1",
+      "failures"=>"1", "schoolsup"=>"yes", "famsup"=>"yes", "paid"=>"yes",
+      "activities"=>"yes", "nursery"=>"yes", "higher"=>"yes", "internet"=>"yes",
+      "romantic"=>"yes", "famrel"=>"3", "freetime"=>"1", "goout"=>"1",
+      "Dalc"=>"4", "Walc"=>"1", "health"=>"3", "absences"=>"7", "G1"=>"17",
+      "G2"=>"7", "G3"=>"18"
+    }
+    get :result, query
+    assert_equal assigns(:result), "At home"
+  end
+
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -14,44 +14,22 @@ class StudentTest < ActiveSupport::TestCase
   end
 
   test "students can be put into id3 tree" do
-    puts @tree.to_s
+    # puts @tree.to_s
   end
 
   test "complete test 1" do
     query = {
-      "sex"=>"F",
-      "age"=>"15",
-      "address"=>"U",
-      "famsize"=>"LE3",
-      "Pstatus"=>"T",
-      "Medu"=>"0",
-      "Fedu"=>"0",
-      "Fjob"=>"teacher",
-      "reason"=>"home",
-      "guardian"=>"mother",
-      "traveltime"=>"1",
-      "studytime"=>"1",
-      "failures"=>"1",
-      "schoolsup"=>"yes",
-      "famsup"=>"yes",
-      "paid"=>"yes",
-      "activities"=>"yes",
-      "nursery"=>"yes",
-      "higher"=>"yes",
-      "internet"=>"yes",
-      "romantic"=>"yes",
-      "famrel"=>"3",
-      "freetime"=>"1",
-      "goout"=>"1",
-      "Dalc"=>"4",
-      "Walc"=>"1",
-      "health"=>"3",
-      "absences"=>"7",
-      "G1"=>"17",
-      "G2"=>"7",
-      "G3"=>"18"
+      "sex"=>"F", "age"=>"15", "address"=>"U", "famsize"=>"LE3", "Pstatus"=>"T",
+      "Medu"=>"0", "Fedu"=>"0", "Fjob"=>"teacher", "reason"=>"home",
+      "guardian"=>"mother", "traveltime"=>"1", "studytime"=>"1",
+      "failures"=>"1", "schoolsup"=>"yes", "famsup"=>"yes", "paid"=>"yes",
+      "activities"=>"yes", "nursery"=>"yes", "higher"=>"yes", "internet"=>"yes",
+      "romantic"=>"yes", "famrel"=>"3", "freetime"=>"1", "goout"=>"1",
+      "Dalc"=>"4", "Walc"=>"1", "health"=>"3", "absences"=>"7", "G1"=>"17",
+      "G2"=>"7", "G3"=>"18"
     }
-    @tree.query query
+    r = @tree.query query
+    assert_equal "at_home", r
   end
 
 end
